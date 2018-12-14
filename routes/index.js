eda1a13a3745608e6da0b504746b72157367fce0
Yunjong Jeong <yunjong@kaist.ac.kr>
2018-12-14 14:27:30 +0900

    Fetch currency rate from the internet

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,27 @@
 var db = require('../db');
 var express = require('express');
+var fetch = require('node-fetch');
 var router = express.Router();
 
-function getCurrencyRate(unit) {
-  var rates = {
-    'KRW': 1,
-    'USD': 1000,
-    'JPY': 100,
-  };
-  return rates[unit];
+var rates = {
+  'KRW': 1,
+  'USD': 0.0001,
+  'JPY': 0.1,
+};
+
+function queryCurrencyRate() {
+  console.log('Querying..');
+  return fetch('https://api.exchangeratesapi.io/latest?base=KRW')
+  .then(res => res.json())
+  .then(res => {
+    rates = res.rates;
+  });
 }
 
 function totalAmount(spendings) {
   var total = 0;
   for (var i=0; i<spendings.length; i++) {
-    total += parseFloat(spendings[i].amount)
-      * getCurrencyRate(spendings[i].unit);
+    total += parseFloat(spendings[i].amount) / rates[spendings[i].unit];
   }
   return total;
 }
@@ -23,8 +29,10 @@ function totalAmount(spendings) {
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var spendings = db.Spending.findAll();
-  var total = totalAmount(spendings);
-  res.render('index', { spendings: spendings, total: total });
+  queryCurrencyRate().then(() => {
+    var total = totalAmount(spendings);
+    res.render('index', { spendings: spendings, total: total });
+  });
 });
 
 /* POST new entry */
